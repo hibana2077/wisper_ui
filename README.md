194c4c857ec84b2bbeb2a6f87ce14c9baf902725
hibana2077 <hibana2077@gmail.com>
2024-06-15 09:50:10 +0800

    feat: Add Whisper logo and description to landing page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wisper_ui
+
+![OpenAI](https://img.shields.io/badge/OpenAI-Wisper-412991?style=plastic-square&logo=OpenAI)
--- src/web/app.py
+++ src/web/app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import pandas as pd
+import torch
 import whisper
 
-MODEL_SIZE_LIST = ["tiny", "base", "small", "medium", "large"]
+# check gpu memory size
+GPU_MEMORY_SIZE = torch.cuda.get_device_properties(0).total_memory / 1024 ** 3
+MODEL_MEM_REQ_DICT = {
+    "tiny": 0.5,
+    "base": 1.5,
+    "small": 2.5,
+    "medium": 8,
+    "large": 12,
+}
+MODEL_SIZE_LIST = [key for key, value in MODEL_MEM_REQ_DICT.items() if value < GPU_MEMORY_SIZE]
 
 @st.cache_resource
 def load_model(size):
@@ -18,6 +28,12 @@ def convert_to_dataframe(result):
 
 st.title("AI 語音轉逐字稿")
 
+st.markdown("""![OpenAI](https://img.shields.io/badge/OpenAI-Wisper-412991?style=plastic-square&logo=OpenAI)""")
+st.write("""這是一個使用 OpenAI 的 Whisper 模型來進行語音轉逐字稿的網頁應用程式。""")
+st.write("""Whisper 是一個基於 Transformer 的端對端語音轉文字模型，可以將語音檔案轉換成逐字稿。""")
+
+
+st.write(f"GPU 記憶體大小: {GPU_MEMORY_SIZE:.2f} GB")
 model_size = st.selectbox("選擇模型大小", MODEL_SIZE_LIST)
 model = load_model(model_size)
 
